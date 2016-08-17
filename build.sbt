8b82f5bca119e2afb70967fb5001c87ed9914157
russell <russell@radioactive.sg>
2016-08-17 10:44:47 +0800

    null checks for user written

--- build.sbt
+++ build.sbt
@@ -5,6 +5,6 @@ version := "1.0"
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
-  "org.scalacheck" %% "scalacheck" % "1.12.5" % "test",
+  "org.scalacheck" %% "scalacheck" % "1.12.1" % "test",
   "org.scalatest" %% "scalatest" % "2.2.5" % "test"
 )
--- src/test/scala/property/UserSpec.scala
+++ src/test/scala/property/UserSpec.scala
@@ -2,23 +2,37 @@ package property
 
 import models.User
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
-import org.scalatest.{MustMatchers, WordSpecLike}
+import org.scalatest.{Assertions, MustMatchers, WordSpecLike}
+import org.scalatest.Assertions._
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Gen
 
-class UserSpec extends WordSpecLike with MustMatchers with GeneratorDrivenPropertyChecks {
+class UserSpec extends WordSpecLike with MustMatchers with GeneratorDrivenPropertyChecks with Assertions {
+
+  val stringOrNull =  Gen.oneOf(Gen.const(null),arbitrary[String])
+
+  val users = for {
+    name <- arbitrary[String]
+    address <- arbitrary[String]
+  } yield new User(name,address)
+
 
   "User" should {
-    "not be constructable with a empty name" in {
-      forAll {
-        (str: String) => {
-          whenever(str.isEmpty) {
-            intercept[IllegalArgumentException] {
-              new User(str)
-            }
+    "not be constructable with null parameters" in {
+      forAll(stringOrNull,stringOrNull) {
+        (name: String, address : String) => {
+          if(name == null || address == null) {
+            intercept[IllegalArgumentException](new User(name,address))
+          }else{
+            new User(name,address)
           }
-
-          whenever(str.nonEmpty) {
-            new User(str)
-          }
+        }
+      }
+    }
+    "compare to other users correctly" in {
+      pending
+      forAll(users,users) {
+        (user1 : User, user2) => {
         }
       }
     }
